54a692c7ba8b71ee306d9440fce6df5d67cb67bc
sacky3105 <kgglee.114.baritone@gmail.com>
2021-09-16 06:35:22 +0900

    feat create docker app container

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('tests/test','TestController@index');
+
+
+Route::group(['prefix'=>'contact','middleware' => 'auth'],function(){
+    Route::get('index', 'ContactFormController@index')->name('contact.index');
+});
+
+
+// REST
+//Route::resource('contacts','ContactFormController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
