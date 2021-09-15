918ae60810a1b6be043b759e3fa25b6394010049
sacky3105 <kgglee.114.baritone@gmail.com>
2021-09-16 07:21:03 +0900

    feat modified task_test

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,1 +1,23 @@
-indexです
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+                    <a href="{{route('contact.create')}}">新規登録</a>
+                    indexです
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
